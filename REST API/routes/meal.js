72d506ec17326291dd2b3a7b9412c7baf26dc21c
unknown <themtrx@mail.bg>
2020-08-06 22:26:20 +0300

    divide meals on published and unpublished

--- REST API/routes/meal.js
+++ REST API/routes/meal.js
@@ -2,7 +2,9 @@ const controllers = require('../controllers');
 const router = require('express').Router();
 const { auth } = require('../utils');
 
-router.get('/', controllers.meal.get);
+router.get('/', controllers.meal.get.published);
+
+router.get('/unpublished', auth(), controllers.meal.get.getUnpublished);
 
 router.post('/', auth(), controllers.meal.post);
 
