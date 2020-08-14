44a865504fa30651802729e5494b4742e8cf74f8
unknown <themtrx@mail.bg>
2020-08-14 14:33:37 +0300

    feat: login and authorization

--- REST API/controllers/user.js
+++ REST API/controllers/user.js
@@ -40,7 +40,7 @@ module.exports = {
                     }
 
                     const token = utils.jwt.createToken({ id: user._id });
-                    res.cookie(config.authCookieName, token).send(user);
+                    res.header('Authorization', token).send(user);
                 })
                 .catch(next);
         },
